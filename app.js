921592dc041f85841dc6a447e23c1e5d11e53dc2
Andrew-Ochieng <andrewomosh44@gmail.com>
2022-09-15 18:54:40 +0300

    add search function

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ const apiUrl = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.d
 const imgPath = "https://image.tmdb.org/t/p/w1280";
 const searchApi = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query=";
 
-
+// initializa variables
 const mainEl = document.getElementById('main')
+const search = document.getElementById('search')
+const myForm = document.getElementById('form')
 
-
-function getMovies() {
-    fetch(apiUrl)
+// display all movies
+function getMovies(url) {
+    fetch(url)
         .then((response) => response.json())
         .then((data) => {
             console.log(data)
+
 
             showMovies(data.results)
     
@@ -19,45 +22,68 @@ function getMovies() {
 }
 
 
+// search movies
 function showMovies(movies) {
     mainEl.innerHTML = ""
 
-    movies.forEach((movie) => {
-    const { poster_path, title, vote_average, overview } = movie;
 
-    mainEl.innerHTML += `
-            <div class="movie">
-                <img id="image" src="${imgPath + poster_path} "
-                <div class="movie-info">
-                    <h3>${title}</h3>
-                    <span id="vote">
-                        ${vote_average}
-                    </span>
+    movies.forEach((movie) => {
+        const { poster_path, title, vote_average } = movie;
+    
+        mainEl.innerHTML += `
+                <div class="movie">
+                    <img id="image" src="${imgPath + poster_path}" alt="${title}"/>
+                    <div class="movie-info">
+                        <h3>${title}</h3>
+                        <span class="${voteColor(vote_average)}">
+                            ${vote_average}
+                        </span>
+                    </div>
+                    
                 </div>
-                
-            </div>
-        `
-        
-    });
+            `
+            
+        });
+    
 }
 
-// const voteColor = document.getElementById('vote').innerHTML
 
-// function changeColor() {
-//     if (voteColor < 7) {
-//         voteColor.style.color = "red"
-//     } else if (voteColor <= 8) {
-//         voteColor.style.color = "orange"
-//     } else {
-//         voteColor.style.color = "green"
-//     }
-// }
+
+myForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    // console.log('proceed')
+    
+
+    const searchTerm = search.value;
+
+    if (searchTerm) {
+        
+        getMovies(searchApi + searchTerm) 
+
+        search.value = ""
+    }
+
+
+})
+
+
+// vote color
+function voteColor(vote) {
+    if (vote >= 8) {
+        return 'green';
+    } else if (vote >= 6) {
+        return 'orange';
+    } else {
+        return 'red';
+    }
+}
 
 
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    getMovies()
+    getMovies(apiUrl)
 
 
 })
